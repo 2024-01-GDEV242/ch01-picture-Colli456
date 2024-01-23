a1239f2f48d31898f238d725b1bf1298d64aec27
Colli456 <cjclarke14y@gmail.com>
2024-01-22 19:02:12 -0500

    P1-Picture Updated
    Update description and name

--- Picture.java
+++ Picture.java
@@ -1,13 +1,11 @@
 /**
- * This class represents a simple picture. You can draw the picture using
- * the draw method. But wait, there's more: being an electronic picture, it
- * can be changed. You can set it to black-and-white display and back to
- * colors (only after it's been drawn, of course).
+ * Creating a picture using a number of squares, triangles, circles 
+ * and persons.
  *
  * This class was written as an early example for teaching Java with BlueJ.
  * 
- * @author  Michael Kšlling and David J. Barnes
- * @version 2016.02.29
+ * @author  Collie Clarke
+ * @version 2024.01.22
  */
 public class Picture
 {
